7a3d2e412f73ed02553afb4bcf4729022383a778
Lionel Kaneza <lionkaneza@gmail.com>
2024-12-23 13:07:43 +0200

    feat: add unicorn eslint plugin to the configurations

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  extends: 'expo',
-  ignorePatterns: ['/dist/*'],
+  extends: ["expo", "plugin:unicorn/recommended"],
+  ignorePatterns: ["/dist/*"],
 };
