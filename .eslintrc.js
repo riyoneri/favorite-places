8ff611aed7b14b64ca3fdf3ccf1f31148c8aa105
Lionel Kaneza <lionkaneza@gmail.com>
2024-12-23 13:10:40 +0200

    feat: add unused-imports eslint plugin to the configurations

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,4 +2,21 @@
 module.exports = {
   extends: ["expo", "plugin:unicorn/recommended"],
   ignorePatterns: ["/dist/*"],
+  plugins: ["unused-imports"],
+  rules: {
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": [
+      "error",
+      {
+        varsIgnorePattern: "^_",
+        args: "all",
+        argsIgnorePattern: "^_",
+        caughtErrorsIgnorePattern: "^_",
+        destructuredArrayIgnorePattern: "^_",
+        ignoreRestSiblings: true,
+      },
+    ],
+  },
 };
