28b8dca64f1f9c98d021e255a69978eac1f37eec
Lionel Kaneza <lionkaneza@gmail.com>
2024-12-24 13:45:27 +0200

    feat: create places list component

---
+++ components/places/places-list.tsx
@@ -0,0 +1,42 @@
+import { FlatList, StyleSheet, Text, View } from "react-native";
+
+import { Colors } from "../../constants/style";
+import { Place } from "../../models/place";
+import PlaceItem from "./place-item";
+
+interface PlacesListProperties {
+  places: Place[];
+}
+
+export default function PlacesList({ places }: PlacesListProperties) {
+  if (places.length === 0) {
+    return (
+      <View style={styles.fallBackContainer}>
+        <Text style={styles.fallBackText}>
+          No places added yet - Start adding some!
+        </Text>
+      </View>
+    );
+  }
+  return (
+    <FlatList
+      data={places}
+      keyExtractor={(item) => item.id}
+      renderItem={(itemData) => (
+        <PlaceItem place={itemData.item} onPress={() => {}} />
+      )}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  fallBackContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  fallBackText: {
+    fontSize: 16,
+    color: Colors.primary200,
+  },
+});
