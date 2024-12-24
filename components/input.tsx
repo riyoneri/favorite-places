8d8580b3695913cec20012a4fa172051ecc424d8
Lionel Kaneza <lionkaneza@gmail.com>
2024-12-24 12:37:23 +0200

    feat: create input component

---
+++ components/input.tsx
@@ -0,0 +1,61 @@
+import { Control, Controller, FieldValues, Path } from "react-hook-form";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TextInputProps,
+  View,
+} from "react-native";
+
+import { Colors } from "../constants/style";
+
+interface InputProperties<T extends FieldValues> {
+  label: string;
+  name: Path<T>;
+  control: Control<T>;
+  inputConfig: TextInputProps;
+}
+
+export default function Input<T extends FieldValues>({
+  label,
+  control,
+  inputConfig,
+  name,
+}: InputProperties<T>) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>{label}</Text>
+      <Controller
+        name={name}
+        control={control}
+        render={({ field: { onBlur, onChange, value } }) => (
+          <TextInput
+            style={styles.input}
+            {...inputConfig}
+            onChangeText={onChange}
+            onBlur={onBlur}
+            value={value}
+          />
+        )}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    gap: 10,
+  },
+  label: {
+    color: Colors.primary500,
+    fontSize: 19,
+  },
+  input: {
+    backgroundColor: Colors.primary100,
+    borderBottomWidth: 3,
+    fontSize: 18,
+    borderBottomColor: Colors.primary700,
+    paddingHorizontal: 7,
+    paddingVertical: 10,
+  },
+});
