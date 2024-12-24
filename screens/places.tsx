017475e51537308f64d45e3daf7d7c199ab33094
Lionel Kaneza <lionkaneza@gmail.com>
2024-12-24 12:40:11 +0200

    feat: add icon-button on places header

--- screens/places.tsx
+++ screens/places.tsx
@@ -1,5 +1,24 @@
+import { useLayoutEffect } from "react";
 import { Text } from "react-native";
 
-export default function PlacesScreen() {
+import IconButton from "../components/ui/icon-button";
+import { RootStackScreenProperties } from "../types/navigation";
+
+export default function PlacesScreen({
+  navigation,
+}: RootStackScreenProperties<"Places">) {
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: ({ tintColor }) => (
+        <IconButton
+          icon="add"
+          color={tintColor}
+          onPress={() => navigation.navigate("AddPlace")}
+          size={27}
+        />
+      ),
+    });
+  }, [navigation]);
+
   return <Text>Places screen</Text>;
 }
