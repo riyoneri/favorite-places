b3b5c6e0d2c97508e551c05a388e0cf47f11f65f
Lionel Kaneza <lionkaneza@gmail.com>
2024-12-24 12:41:37 +0200

    feat: create screen-wrapper component

---
+++ components/ui/screen-wrapper.tsx
@@ -0,0 +1,17 @@
+import { PropsWithChildren } from "react";
+import { StyleSheet, View } from "react-native";
+
+import { Colors } from "../../constants/style";
+
+export default function ScreenWrapper({ children }: PropsWithChildren) {
+  return <View style={styles.container}>{children}</View>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 16,
+    paddingTop: 16,
+    backgroundColor: Colors.gray700,
+  },
+});
