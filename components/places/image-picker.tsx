70caedeb2b587d465c9c20eb3fa2709aad228d2c
Lionel Kaneza <lionkaneza@gmail.com>
2024-12-25 10:44:59 +0200

    feat: implement image picker functionality

---
+++ components/places/image-picker.tsx
@@ -0,0 +1,83 @@
+import {
+  launchCameraAsync,
+  PermissionStatus,
+  useCameraPermissions,
+} from "expo-image-picker";
+import { useState } from "react";
+import { Alert, Image, StyleSheet, Text, View } from "react-native";
+
+import { Colors } from "../../constants/style";
+import OutlinedButton from "../ui/outlined-button";
+
+export default function ImagePicker() {
+  const [pickedImage, setPickedImage] = useState("");
+  const [cameraPermissionInformation, requestPermission] =
+    useCameraPermissions();
+
+  async function verifyPermissions() {
+    if (cameraPermissionInformation?.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
+
+      return permissionResponse.granted;
+    }
+
+    if (cameraPermissionInformation?.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "Insufficient Permissions!",
+        "You need to grant camera permissions to use this app",
+      );
+
+      return false;
+    }
+
+    return true;
+  }
+
+  async function pickImageHandler() {
+    const hasPermission = await verifyPermissions();
+
+    if (!hasPermission) return;
+
+    const image = await launchCameraAsync({
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 0.5,
+    });
+
+    if (image.canceled) return;
+
+    setPickedImage(image.assets[0].uri);
+  }
+
+  return (
+    <View>
+      <View style={styles.imagePreview}>
+        {pickedImage ? (
+          <Image style={styles.image} source={{ uri: pickedImage }} />
+        ) : (
+          <Text>No image taken yet.</Text>
+        )}
+      </View>
+      <OutlinedButton onPress={pickImageHandler} icon="camera">
+        Pick Image
+      </OutlinedButton>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  imagePreview: {
+    width: "100%",
+    height: 200,
+    marginVertical: 8,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.primary100,
+    borderRadius: 4,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    objectFit: "cover",
+  },
+});
