09a9414af8e362d4f2017ccf6c48fee947225918
Lionel Kaneza <lionkaneza@gmail.com>
2024-12-25 17:49:16 +0200

    feat: extend navigation route parameters

--- types/navigation.tsx
+++ types/navigation.tsx
@@ -1,15 +1,24 @@
 /* eslint-disable unicorn/prevent-abbreviations */
+import { ParamListBase, RouteProp } from "@react-navigation/native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
+
+import { Location } from "../api";
 
 export type RootStackParametersList = {
   Places: undefined;
   PlacesDetail: { placeId: string };
-  AddPlace: undefined;
-  Map?: { placeId: string };
+  AddPlace?: {
+    location: Location;
+  };
+  Map?: { location: Location };
 };
 
 export type RootStackScreenProperties<T extends keyof RootStackParametersList> =
   NativeStackScreenProps<RootStackParametersList, T>;
+
+export type RootStackScreenRouteProperties<
+  T extends keyof RootStackParametersList,
+> = RouteProp<ParamListBase, T> & { params: RootStackParametersList[T] };
 
 declare global {
   namespace ReactNavigation {
