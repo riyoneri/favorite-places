9e99834104af7920804b89c17b304fee09b4ba86
Lionel Kaneza <lionkaneza@gmail.com>
2024-12-25 17:50:53 +0200

    feat: forward selected location to the form

--- screens/map.tsx
+++ screens/map.tsx
@@ -1,7 +1,51 @@
-import { StyleSheet } from "react-native";
-import MapView from "react-native-maps";
+import { useLayoutEffect, useState } from "react";
+import { Alert, StyleSheet } from "react-native";
+import MapView, { MapPressEvent, Marker } from "react-native-maps";
 
-export default function MapScreen() {
+import { Location } from "../api";
+import IconButton from "../components/ui/icon-button";
+import { RootStackScreenProperties } from "../types/navigation";
+
+export default function MapScreen({
+  navigation,
+}: RootStackScreenProperties<"Map">) {
+  const [selectedLocation, setSelectedLocation] = useState<
+    Location | undefined
+  >();
+
+  function selectLocationHandler(event: MapPressEvent) {
+    setSelectedLocation({
+      latitude: event.nativeEvent.coordinate.latitude,
+      longitude: event.nativeEvent.coordinate.longitude,
+    });
+  }
+
+  useLayoutEffect(() => {
+    function navigationToFormHandler() {
+      if (!selectedLocation) {
+        Alert.alert(
+          "No location picked",
+          "You have to pick a location by tapping on a map first",
+        );
+
+        return;
+      }
+
+      navigation.navigate("AddPlace", { location: selectedLocation });
+    }
+
+    navigation.setOptions({
+      headerRight: ({ tintColor }) => (
+        <IconButton
+          icon="save"
+          color={tintColor}
+          size={24}
+          onPress={navigationToFormHandler}
+        />
+      ),
+    });
+  }, [navigation, selectedLocation]);
+
   return (
     <MapView
       style={styles.map}
@@ -11,7 +55,10 @@ export default function MapScreen() {
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421,
       }}
-    />
+      onPress={selectLocationHandler}
+    >
+      {selectedLocation && <Marker coordinate={selectedLocation} />}
+    </MapView>
   );
 }
 
