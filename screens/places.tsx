44b528ca6b899e6c690121198efca5a3ff1234c0
Lionel Kaneza <lionkaneza@gmail.com>
2024-12-25 10:46:05 +0200

    feat: display places on allPlaces screen

--- screens/places.tsx
+++ screens/places.tsx
@@ -1,24 +1,11 @@
-import { useLayoutEffect } from "react";
-import { Text } from "react-native";
+import { View } from "react-native";
 
-import IconButton from "../components/ui/icon-button";
-import { RootStackScreenProperties } from "../types/navigation";
+import PlacesList from "../components/places/places-list";
 
-export default function PlacesScreen({
-  navigation,
-}: RootStackScreenProperties<"Places">) {
-  useLayoutEffect(() => {
-    navigation.setOptions({
-      headerRight: ({ tintColor }) => (
-        <IconButton
-          icon="add"
-          color={tintColor}
-          onPress={() => navigation.navigate("AddPlace")}
-          size={27}
-        />
-      ),
-    });
-  }, [navigation]);
-
-  return <Text>Places screen</Text>;
+export default function PlacesScreen() {
+  return (
+    <View style={{ flex: 1 }}>
+      <PlacesList places={[]} />
+    </View>
+  );
 }
