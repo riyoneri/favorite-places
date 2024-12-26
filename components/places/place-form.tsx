24fcf1e66475e8fab72aa671bf07f977d6abed11
Lionel Kaneza <lionkaneza@gmail.com>
2024-12-26 08:43:07 +0200

    feat: wrap form in FormProvider

--- components/places/place-form.tsx
+++ components/places/place-form.tsx
@@ -1,23 +1,31 @@
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { FormProvider, useForm } from "react-hook-form";
 import { ScrollView, StyleSheet } from "react-native";
 
 import { placeSchema, PlaceSchemaType } from "../../validation/place-schema";
 import Input from "../input";
+import Button from "../ui/button";
 import ImagePicker from "./image-picker";
 import LocationPicker from "./location-picker";
 
+function addPlaceHandler() {}
+
 export default function PlaceForm() {
-  const { control } = useForm<PlaceSchemaType>({
+  const methods = useForm<PlaceSchemaType>({
     resolver: zodResolver(placeSchema),
   });
 
   return (
-    <ScrollView style={styles.form}>
-      <Input control={control} name="name" inputConfig={{}} label="Title" />
-      <ImagePicker />
-      <LocationPicker />
-    </ScrollView>
+    <FormProvider {...methods}>
+      <ScrollView style={styles.form}>
+        <Input control={methods.control} name="name" label="Title" />
+        <ImagePicker />
+        <LocationPicker />
+        <Button onPress={methods.handleSubmit(addPlaceHandler)}>
+          Add Place
+        </Button>
+      </ScrollView>
+    </FormProvider>
   );
 }
 
