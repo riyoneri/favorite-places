e4283b97934e5525ec75098ad38ce8069a7ef243
Lionel Kaneza <lionkaneza@gmail.com>
2024-12-26 08:44:54 +0200

    feat: add address in place validation schema

--- validation/place-schema.ts
+++ validation/place-schema.ts
@@ -1,7 +1,13 @@
 import { z } from "zod";
 
 export const placeSchema = z.object({
-  name: z.string().min(3, "must be atlease 3 charcters"),
+  name: z.string().min(3, "Name must be atleast 3 charcters"),
+  imageUri: z.string().min(3, "Image url must be atleast 3 characters"),
+  location: z.object({
+    latitude: z.number(),
+    longitude: z.number(),
+  }),
+  address: z.string().min(3, "Address must be atleast 3 characters"),
 });
 
 export type PlaceSchemaType = z.infer<typeof placeSchema>;
