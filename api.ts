161df8c11d31314380dfd1c3fc85dc06df809571
Lionel Kaneza <lionkaneza@gmail.com>
2024-12-26 13:58:57 +0200

    feat: create DatabasePlace interface

--- api.ts
+++ api.ts
@@ -1,4 +1,11 @@
+import { Place } from "./models/place";
+
 export interface Location {
   latitude: number;
   longitude: number;
+}
+
+export interface DatabasePlace extends Omit<Place, "location"> {
+  lat: number;
+  lng: number;
 }
